75473ad3af98929b3ab8cdaa90f6bcb056e286dc
nhlnhl <nhlnhl@naver.com>
2019-09-15 22:12:12 +0900

    Update hyelim.cpp

--- 08_DFS_BFS/01/hyelim.cpp
+++ 08_DFS_BFS/01/hyelim.cpp
@@ -7,23 +7,23 @@ using namespace std;
 int solution(vector<int> numbers, int target) {
     int answer = 0;
 
-	int vlen = numbers.size();
-	int rlen = pow(2, vlen);
-	int* result = new int[rlen];
-	memset(result, 0, sizeof(int) * rlen);
-	for (int i = 0; i < vlen; i++) {
-		for (int j = 0; j < rlen; j++) {
-			if ((j / (int)(rlen / pow(2, i + 1))) % 2 == 0)
-				result[j] += numbers[i];
-			else
-				result[j] -= numbers[i];
-		}
-	}
+    int vlen = numbers.size();
+    int rlen = pow(2, vlen);
+    int* result = new int[rlen];
+    memset(result, 0, sizeof(int) * rlen);
+    for (int i = 0; i < vlen; i++) {
+        for (int j = 0; j < rlen; j++) {
+            if ((j / (int)(rlen / pow(2, i + 1))) % 2 == 0)
+                result[j] += numbers[i];
+            else
+                result[j] -= numbers[i];
+        }
+    }
 
-	for (int i = 0; i < rlen; i++) {
-		if (result[i] == target)
-			answer++;
-	}
+    for (int i = 0; i < rlen; i++) {
+        if (result[i] == target)
+            answer++;
+    }
 
-	return answer;
+    return answer;
 }
