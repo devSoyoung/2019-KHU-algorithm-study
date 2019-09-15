46d414b1ebf008a24dfbfccfe7a39c9f26492c0e
nhlnhl <nhlnhl@naver.com>
2019-09-15 22:08:25 +0900

    Update hyelim.cpp

--- 02_Stack_Queue/01/hyelim.cpp
+++ 02_Stack_Queue/01/hyelim.cpp
@@ -1,40 +1,39 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <utility>
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
-    
+
     int length = priorities.size();
-	deque<pair<int, int>> queue;
-	for (int i = 0; i < length; i++)
-		queue.push_back(make_pair(i, priorities[i]));
+    deque<pair<int, int>> queue;
+    for (int i = 0; i < length; i++)
+        queue.push_back(make_pair(i, priorities[i]));
 
-	int count = 0;
-	while(1) {
-		bool check = true;
-		pair<int, int> temp = queue.at(0);
-		int comparand = temp.second;
-		for (deque<pair<int, int>>::iterator iter = queue.begin(); iter < queue.end(); iter++) {
-			if (comparand < (*iter).second) {
-				queue.pop_front();
-				queue.push_back(temp);
-				check = false;
-				break;
-			}
-		}
+    int count = 0;
+    while(1) {
+        bool check = true;
+        pair<int, int> temp = queue.at(0);
+        int comparand = temp.second;
+        for (deque<pair<int, int>>::iterator iter = queue.begin(); iter < queue.end(); iter++) {
+            if (comparand < (*iter).second) {
+                queue.pop_front();
+                queue.push_back(temp);
+                check = false;
+                break;
+            }
+        }
 
-		if (check) {
-			queue.pop_front();
-			count++;
-			if (temp.first == location) {
-				answer = count;
-				break;
-			}
-		}
-	}
+        if (check) {
+            queue.pop_front();
+            count++;
+            if (temp.first == location) {
+                answer = count;
+                break;
+            }
+        }
+    }
 
-	return answer;
+    return answer;
 }
