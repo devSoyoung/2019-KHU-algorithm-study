40be114693f32df31509140659c9af0e16f86144
nhlnhl <nhlnhl@naver.com>
2019-09-15 21:44:08 +0900

    Create hyelim.cpp
    No stack or queue..?

---
+++ 02_Stack_Queue/02/hyelim.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> heights) {
+    vector<int> answer;
+    
+    answer.push_back(0);
+	int length = heights.size();
+	for (int i = 1; i < length; i++) {
+		for (int j = i - 1; j >= 0; j--) {
+			if (heights[j] > heights[i]) {
+				answer.push_back(j + 1);
+				break;
+			}
+			if (j == 0)
+				answer.push_back(0);
+		}
+	}
+
+	return answer;
+}
